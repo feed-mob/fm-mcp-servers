14599b0aa424f938702999dbeb20884c38c9afcb
mandy-feedmob <mandy@feedmob.com>
2026-01-28 16:27:08 +0800

    update

--- src/iplocate/src/index.ts
+++ src/iplocate/src/index.ts
@@ -8,7 +8,7 @@ import { lookupIP, batchLookup, fraudCheck, batchFraudCheck, getUsageStats, refr
 // Create server instance
 const server = new McpServer({
   name: "iplocate",
-  version: "0.0.1"
+  version: "0.0.2"
 });
 
 // Tool Definition for Single IP Lookup
